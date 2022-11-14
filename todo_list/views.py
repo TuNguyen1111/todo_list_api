8cdf028a21d2ac09e5a4fca07946cc4564b5f9d2
tunv <tunguyen060811@gmail.com>
2022-11-14 21:50:59 +0700

    create overview and task list

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Task
+from .serializers import TaksSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def api_overview(request):
+    response = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/'
+    }
+    return Response(response)
+
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaksSerializer(tasks, many=True)
+    return Response(serializer.data)
